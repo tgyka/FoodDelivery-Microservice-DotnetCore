1685147d071a4a26ffd5e11048aee1025b3ef5f6
tgyka <tugaykaramustafa@windowslive.com>
2022-06-05 15:23:47 +0300

    Add project files.

---
+++ Services/NotificationService/FoodDelivery.Notification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodDelivery.Notification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Services/PaymentService/FoodDelivery.Payment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodDelivery.Payment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
