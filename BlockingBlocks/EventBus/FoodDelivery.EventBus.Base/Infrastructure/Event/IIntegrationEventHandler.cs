fcddd441319a623acd68716e74372e7ed2402c67
tgyka <tugaykaramustafa@windowslive.com>
2022-06-07 16:49:52 +0300

    EventBus infrastructure completed

---
+++ BlockingBlocks/EventBus/FoodDelivery.EventBus.Base/Infrastructure/Event/IIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.EventBus.Base.Infrastructure.Event
+{
+    public interface IIntegrationEventHandler<T> 
+        where T : IntegrationEvent 
+    {
+        Task Handle(T @event);
+    }
+}
---
+++ BlockingBlocks/EventBus/FoodDelivery.EventBus.Base/Infrastructure/IEventBusSubsManager.cs
@@ -0,0 +1,26 @@
+﻿using FoodDelivery.EventBus.Base.Infrastructure.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.EventBus.Base.Infrastructure
+{
+    public interface IEventBusSubsManager
+    {
+        void AddSubs<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+
+        void RemoveSubs<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+
+        void Clear();
+
+        bool HasSubsForEvent<T>() where T : IntegrationEvent;
+
+
+    }
+}
